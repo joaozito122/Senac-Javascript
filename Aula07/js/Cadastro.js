a19c1e0a23b76dac1c266061a379daeb49eb3be5
Joao vitor <Joaovitordebessa17@gmail.com>
2024-09-21 12:01:40 -0300

    Funções anonimas e arrow function

---
+++ Aula07/js/Cadastro.js
@@ -0,0 +1,49 @@
+let nome = document.getElementById("txtnome")
+let email = document.getElementById("txtemail")
+let telefone = document.getElementById("txttel")
+let idade = document.getElementById("txtidade")
+
+
+let dados = []
+let cadastro = () =>{
+    let cliente = {
+        nome:nome.value,
+        email:email.value,
+        telefone:telefone.value,
+        idade:idade.value
+
+    }
+    
+    dados.push(cliente)
+    listarClientes()
+    console.log(dados)
+}
+
+let listarClientes = () =>{
+    let lista = document.getElementById("lista")
+    lista.innerHTML=`
+    <p>
+        <span>Nome</span>
+        <span>E-mail</span>
+        <span>Telefone</span>
+        <span>Idade</span>
+    </p>
+    `
+
+    let saida = ""
+    dados.map((cli,ix)=>{
+
+        saida += `
+        <p>
+            <span>${cli.nome}</span>
+            <span>${cli.email}</span>
+            <span>${cli.telefone}</span>
+            <span>${cli.idade}</span>
+        </p>
+        `
+        
+})
+    
+    lista.innerHTML += saida
+
+}
---
+++ Aula07/js/index.js
@@ -0,0 +1,12 @@
+let somadoisvalores = function(x,y){
+    alert("Foi feita a soma de " +x+ " e " +y+ " igual " +(x+y))
+}
+
+let raizQua = function(valor){
+    alert(`A raiz quadrada de ${valor} é ${Math.sqrt(valor)}`)
+}
+
+let potencia = (base, exp) => {
+    let rs = Math.pow(base, exp)
+    alert(`O resultado é ${rs}`)
+}
